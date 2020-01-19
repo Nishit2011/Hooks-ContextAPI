5f953d44ecd2e84358dea4e17b78b0da3a607cc9
Nishit Ranjan <nishranjan@deloitte.com>
2020-01-19 11:17:34 +0530

    creating context provider in functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,6 @@
-import React, { Component, useContext } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { BookContext } from "../contexts/BookContext";
 
 //using context to access data parent component
 //below is a class component
@@ -29,6 +30,7 @@ import { ThemeContext } from "../contexts/ThemeContext";
 //below is a functional component
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
 
   return (
@@ -37,9 +39,13 @@ const BookList = () => {
       style={{ color: theme.syntax, background: theme.bg }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {books.map(book => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
